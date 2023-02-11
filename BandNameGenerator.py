87dc569f06daaa1fce20acc7faceb76e18536cd4
s-raagavee <59675368+s-raagavee@users.noreply.github.com>
2023-02-11 00:38:12 +0000

    Day 1 project
    Band name generator using users city name and pet name and combining them to make the band name.

---
+++ BandNameGenerator.py
@@ -0,0 +1,14 @@
+#AUTHOR: Raagavee Selvamohan
+#Day 1 Create a Band name using the users city name and pet name and combining them to make teh band name.
+
+#Create a greeting for your program.
+print("Welcome to the Band Name Generator.")
+
+#Ask the user for the city that they grew up in.
+city = input("What's the name of the city you grew up in? \n")
+
+#Ask the user for the name of a pet.
+pet_name = input("What is the name of your pet? \n")
+
+#Combine the name of their city and pet and show them their band name.
+print("Your band name is " + city + " " + pet_name)
